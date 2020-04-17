1e8645b83c0ce36544b523511b9358e8e66527d7
ziming-xixi <gk562501613@163.com>
2020-04-18 00:21:31 +0800

    【功能新增】新增javastudydemo模块 【开发周期】1 【评审人】gk 【提交人】gk

---
+++ javastudydemo/src/main/java/pers/gk/javastudydemo/JavastudydemoApplication.java
@@ -0,0 +1,13 @@
+package pers.gk.javastudydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastudydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavastudydemoApplication.class, args);
+	}
+
+}
